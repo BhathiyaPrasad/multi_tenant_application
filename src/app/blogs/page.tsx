cd6f2f62480a361daaa8e3ca02769eda3ebd117e
I.A.B.P Rathnayake <90783913+BhathiyaPrasad@users.noreply.github.com>
2025-07-14 03:55:40 +0530

    create a new page for view all the blogs

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+
+import {useEffect , useState} from "react";
+import { PublicBlogList } from "@/components/public-blogs";
+import {Navbar} from "@/components/navbar";
+
+
+
+export default function BlogPage() {
+
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const getBlogs = async () => {
+      try {
+        const response = await fetch('/api/public/blogs');
+        const data = await response.json();
+        setBlogs(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error checking session:", error);
+      }
+    };
+
+    getBlogs().then(r => console.log("Blogs checked successfully"));
+  }, []);
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar/>
+      <div className="flex-1 m-10">
+        <PublicBlogList blogs={blogs} />
+      </div>
+    </div>
+  )
+}
