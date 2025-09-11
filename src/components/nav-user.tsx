97ab12401ba76770d79252f9f92d70e81e7b8255
I.A.B.P Rathnayake <90783913+BhathiyaPrasad@users.noreply.github.com>
2025-09-11 17:11:03 +0530

    update the seciotn check call and retireve the user info from token

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -28,17 +28,42 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar"
+import {useEffect, useState} from "react";
+
+type User = {
+  name: string
+  email: string
+  avatar: string
+  tenantName: string
+  tenantId: string
+}
+
+
 
 export function NavUser({
-  user,
-}: {
-  user: {
-    name: string
-    email: string
-    avatar: string
-  }
+
 }) {
+
   const { isMobile } = useSidebar()
+  const [user, setUser] = useState<User | null>(null)
+
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+            try{
+                const response = await fetch("/api/auth/session-check")
+                const data = await response.json()
+                console.log(data)
+                setUser(data)
+
+            }catch{
+                console.log("Error getting user details")
+            }
+        }
+        fetchUserDetails().then(r => console.log("Then"))
+    }, []);
+
+
+
 
   return (
     <SidebarMenu>
@@ -50,13 +75,13 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg grayscale">
-                <AvatarImage src={user.avatar} alt={user.name} />
+                {/*<AvatarImage src={user.avatar} alt={user.name} />*/}
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
+                <span className="truncate font-medium">{user?.name}</span>
                 <span className="text-muted-foreground truncate text-xs">
-                  {user.email}
+                  {user?.email}
                 </span>
               </div>
               <IconDotsVertical className="ml-auto size-4" />
@@ -71,13 +96,13 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
+                  {/*<AvatarImage src={user.avatar} alt={user.name} />*/}
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="truncate font-medium">{user?.name}</span>
                   <span className="text-muted-foreground truncate text-xs">
-                    {user.email}
+                    {user?.email}
                   </span>
                 </div>
               </div>
