f8532ec3b2a3a2c0c5acdb2043b787cd99e311d0
I.A.B.P Rathnayake <90783913+BhathiyaPrasad@users.noreply.github.com>
2025-09-11 12:18:15 +0530

    Pass New prop for update the blog list right after a blog craeted or deleted

--- src/app/(tenant)/dashboard/page.tsx
+++ src/app/(tenant)/dashboard/page.tsx
@@ -44,7 +44,7 @@ export default function Page() {
                 <div className="flex flex-1 flex-col">
                     <div className="@container/main flex flex-1 flex-col gap-2">
                         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                            <SectionCards blogs={blogs}/>
+                            <SectionCards  onBlogUpdated={getPrivateBlogs}  blogs={blogs}/>
                             <div className="px-4 lg:px-6">
 
                             </div>
