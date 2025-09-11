5a1da01a543999b4bd68942e99b29050fef7c6bd
I.A.B.P Rathnayake <90783913+BhathiyaPrasad@users.noreply.github.com>
2025-09-11 16:25:56 +0530

    remove

--- src/app/lib/tenant.ts
+++
@@ -1,8 +0,0 @@
-import { headers } from 'next/headers'
-
-export async function getTenantSlugFromHeader() {
-    const headersList =  await headers() // ✅ no await needed
-    const slug = headersList.get('x-tenant-slug')
-    if (!slug) throw new Error('Missing tenant header')
-    return slug
-}
